d6bbb0fc76a331423bc12f8ae0f0168ab5bf7f6a
rafavanzo <rafacvanzo@hotmail.com>
2023-12-03 20:06:03 -0300

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   resources :pessoas, only: :create
   resource :session, only: :show, path: "login"
-  resources :aulas, only: %i[index create show destroy]
-  resources :aulas_pessoas, only: %i[create index show]
+  resources :aulas, only: %i[index create show destroy], path: "aulas"
+  resources :aulas_pessoas, only: %i[create index show], path: "aulas/:pessoa_id/aulas_pessoas"
 end
